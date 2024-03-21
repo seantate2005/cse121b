fc00948a7f1b13ae2c13784d5f77f7e1888fec97
seantate2005 <161382219+seantate2005@users.noreply.github.com>
2024-03-21 19:39:32 -0400

    d

---
+++ w04-task.js
@@ -0,0 +1,34 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    name: "Sean Tate"
+    photo: images/profile.png;
+    favoriteFoods: [
+
+    ]
+};
+
+
+
+/* Populate Profile Object with placesLive objects */
+
+
+
+
+/* DOM Manipulation - Output */
+
+/* Name */
+
+/* Photo with attributes */
+
+
+/* Favorite Foods List*/
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
